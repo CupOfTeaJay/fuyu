61ea064f62ef9dce04c748efeb48887329efcbb8
Thomas Given <github.backslid766@passmail.net>
2026-01-10 11:20:39 -0500

    POC: compiling a mods workspace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+/*
+    Fuyu
+    Copyright (c) 2026-2026 whoami™ LLC
+
+    TODO: document.
+*/
+
+use std::io::{self, Write};
+use std::path::Path;
+use std::process::Command;
+
+/// TODO: Document.
+fn compile(mods: &Path) {
+    let output = Command::new("cargo")
+        .args([
+            "build",
+            "--manifest-path",
+            mods.join("Cargo.toml").to_str().unwrap(),
+        ])
+        .output()
+        .unwrap();
+    // println!("status: {}", output.status);
+    let _ = io::stdout().write_all(&output.stdout);
+    let _ = io::stderr().write_all(&output.stderr);
+}
+
 fn main() {
-    println!("Hello, world!");
+    compile(Path::new("mods"))
 }
